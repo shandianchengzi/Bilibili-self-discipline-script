e4f12edfb114232156d21c7748a3a31615208db3
shandianchengzi <1252402849@qq.com>
2023-12-10 13:07:24 +0800

    new

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         b站自律脚本
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  1.除了搜索、视频页、私信页之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。
+// @author       shandianchengzi
+// @match        https://www.bilibili.com/*
+// @icon         https://cdn2.iconfinder.com/data/icons/project-management-24/48/62-1024.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
