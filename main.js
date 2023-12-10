785f27a84a3bd81c2ea61a94d7e77a0487ff82de
shandianchengzi <1252402849@qq.com>
2023-12-10 23:53:14 +0800

    1: add my own space as expection.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         b站自律脚本
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  1.除了搜索、视频页、私信页之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。(使用时请打开“设置”-“通用”-“仅在顶层页面（框架）运行”，否则会加载次数过多)
+// @version      0.2
+// @description  1.除了搜索、视频页、私信页、个人主页之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。(使用时请打开“设置”-“通用”-“仅在顶层页面（框架）运行”，否则会加载次数过多)
 // @author       shandianchengzi
 // @match        https://*.bilibili.com/*
 // @icon         https://cdn2.iconfinder.com/data/icons/project-management-24/48/62-1024.png
@@ -24,6 +24,27 @@ async function removeAds(){
     console.log('removeAds success');
 }
 
+
+// check if it is my space page, use the link and cookie to check
+async function checkSpaceLink(link) {
+    var checkSpaceLink = false;
+    var uidArr = link.split('space.bilibili.com/');
+    // use '/' or '?' to split the link
+    var uid = uidArr[1].split('/')[0].split('?')[0];
+    console.log('Check uid', uid);
+    // get cookie
+    var cookie = document.cookie;
+    // get DedeUserID
+    var DedeUserID = cookie.split('DedeUserID=')[1].split(';')[0];
+    console.log('DedeUserID', DedeUserID);
+    // compare uid and DedeUserID
+    if (DedeUserID == uid) {
+        checkSpaceLink = true;
+    }
+    return checkSpaceLink;
+}
+
+
 // check if it is my video page, use the href of class 'up-avatar' and 'header-entry-avatar' to check
 async function checkMyVideoPage() {
     return new Promise((resolve, reject) => {
@@ -34,17 +55,7 @@ async function checkMyVideoPage() {
         var upAvatarHref = upAvatar[0].href;
         console.log('upAvatarHref', upAvatarHref);
         // split the href to get the uid
-        var upAvatarHrefArr = upAvatarHref.split('/');
-        var uid = upAvatarHrefArr[upAvatarHrefArr.length - 1];
-        // get cookie
-        var cookie = document.cookie;
-        // get DedeUserID
-        var DedeUserID = cookie.split('DedeUserID=')[1].split(';')[0];
-        console.log('DedeUserID', DedeUserID);
-        // compare uid and DedeUserID
-        if (DedeUserID == uid) {
-            myVideoPage = true;
-        }
+        myVideoPage = checkSpaceLink(upAvatarHref);
         resolve(myVideoPage);
     });
 }
@@ -77,12 +88,12 @@ async function try_catch_function(func) {
 
 
 async function mainFunc(){
-    //针对b站的脚本，1.除了搜索、视频页、私信页之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。
     // for bilibili
     // not redirect:
     // 1. search: https://search.bilibili.com/
     // 2. video: https://www.bilibili.com/video/
     // 3. message: https://message.bilibili.com/
+    // 4. space: https://space.bilibili.com/
     console.log('usualFunc');
     var url = window.location.href;
     var urlArr = url.split('/');
@@ -116,6 +127,19 @@ async function mainFunc(){
     else if (urlArr[2] == 'message.bilibili.com') {
         console.log('message page');
         // do nothing
+    }
+    // 4. for space page,
+    else if (urlArr[2] == 'space.bilibili.com') {
+        var isMyPage = await checkSpaceLink(url);
+        // for my space page, do nothing
+        if (isMyPage == true) {
+            console.log('my space page');
+            // do nothing
+        }
+        // for other space page, redirect to search page
+        else if (isMyPage == false) {
+            window.location.href = 'https://search.bilibili.com/';
+        }
     }
     // for other pages, redirect to search page
     else {
