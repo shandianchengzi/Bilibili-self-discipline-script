d67223fa25dc1042a4e5a709a096e57180e2dab8
shandianchengzi <1252402849@qq.com>
2024-11-30 20:32:06 +0800

    fix and feat(js): 更新脚本以适应最新版本的b站网页结构，并增加了专栏的忽略。

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         b站自律脚本
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  1.除了搜索、视频页、私信页、个人主页之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。(使用时请打开“设置”-“通用”-“仅在顶层页面（框架）运行”，否则会加载次数过多)
+// @version      0.3
+// @description  1.除了搜索、视频页、私信页、个人主页、专栏之外的任何页都会被重定向到搜索页；2.视频页去掉相关推荐，并且非自己的视频去掉评论。(使用时请打开“设置”-“通用”-“仅在顶层页面（框架）运行”，否则会加载次数过多)
 // @author       shandianchengzi
 // @match        https://*.bilibili.com/*
 // @icon         https://cdn2.iconfinder.com/data/icons/project-management-24/48/62-1024.png
 // @grant        none
 // ==/UserScript==
 
+var skips = {
+    2: ['message.bilibili.com', 'member.bilibili.com'], // 这些是个人消息管理的一些域名，我是UP主，所以需要查看这些，所以不禁用这些域名
+    3: ['opus', 'read'] // opus and read is 专栏
+};
+
 // remove ads
 async function removeAds(){
-    var vcd = document.getElementsByClassName('vcd');
+    var vcd = document.getElementsByClassName('vcd') || document.getElementsByClassName('ad-floor-cover');
     if (vcd.length > 0) {
         for (var i = 0; i < vcd.length; i++) {
             vcd[i].style.display = 'none';
@@ -22,6 +27,11 @@ async function removeAds(){
         vote.style.display = 'none';
     }
     console.log('removeAds success');
+    // remove inside-wrp TODO: because 'index.js:3029  [Deprecation]Listener added for a 'DOMSubtreeModified' mutation event. Support for this event type has been removed, and this event will no longer be fired. See https://chromestatus.com/feature/5083947249172480 for more information.' so it is deprecated, 我现在还没找到怎么去掉这个banner的方式
+    var inside_wrp = document.getElementsByClassName('inside-wrp');
+    if (inside_wrp.length > 0) {
+        inside_wrp[0].style.display = 'none';
+    }
 }
 
 
@@ -75,7 +85,7 @@ async function try_catch_function(func) {
         try {
             var result = await func();
             console.log('try_catch_function success');
-            break; 
+            break;
         } catch (err) {
             console.log('try_catch_function failed', err);
             count += 1;
@@ -107,7 +117,11 @@ async function mainFunc(){
         console.log('video page');
         // remove related videos and other nouseful things
         // 2.1 remove //*[@id="reco_list"] is the related videos
-        var relatedVideos = document.getElementById('reco_list');
+        var relatedVideos = document.getElementById('reco_list') || document.getElementsByClassName('recommend-list-v1');
+        // 如果得到的结果是个列表就取第一个，但是如果没有length属性，就不是列表，就不用取第一个
+        if (relatedVideos.length != undefined && relatedVideos.length > 0) {
+            relatedVideos = relatedVideos[0];
+        }
         relatedVideos.style.visibility = 'hidden';
         // 2.2 remove ads
         await try_catch_function(removeAds);
@@ -115,41 +129,64 @@ async function mainFunc(){
         // for not my video page, remove comments
         if (isMyVideoPage == false) {
             // remove //*[@id="comment"]
-            var comment = document.getElementById('comment');
+            var comment = document.getElementById('comment') || document.getElementById('commentapp');
             comment.style.display = 'none';
         }
-        // 2.2 for my video page, remove comments
+        // 2.3 for my video page, remove comments
         else {
             console.log('my video page');
         }
-    }
-    // 3. for message page,
-    else if (urlArr[2] == 'message.bilibili.com') {
-        console.log('message page');
-        // do nothing
-    }
-    // 4. for space page,
-    else if (urlArr[2] == 'space.bilibili.com') {
-        var isMyPage = await checkSpaceLink(url);
-        // for my space page, do nothing
-        if (isMyPage == true) {
-            console.log('my space page');
-            // do nothing
+        // 2.4 remove live, pop-live-small-mode
+        var live_small = document.getElementsByClassName('live-small-mode');
+        if (live_small.length > 0) {
+            live_small[0].style.display = 'none';
         }
-        // for other space page, redirect to search page
-        else if (isMyPage == false) {
+        // 2.5 remove slide_ad
+        var slide_ad = document.getElementById('slide_ad');
+        if (slide_ad != null) {
+            slide_ad.style.display = 'none';
+        }
+    }
+    // for other pages
+    else{
+        // 3. for all the skip pages and skip domains,
+        for (var key in skips) {
+            for (var i = 0; i < skips[key].length; i++) {
+                if (urlArr[key] == skips[key][i]) {
+                    console.log('skip page');
+                    // do nothing
+                    return;
+                }
+            }
+        }
+
+        // 4. for space page,
+        if (urlArr[2] == 'space.bilibili.com') {
+            var isMyPage = await checkSpaceLink(url);
+            // for my space page, do nothing
+            if (isMyPage == true) {
+                console.log('my space page');
+                // do nothing
+            }
+            // for other space page, redirect to search page
+            else if (isMyPage == false) {
+                window.location.href = 'https://search.bilibili.com/';
+            }
+        }
+        // for other pages, redirect to search page
+        else {
+            console.log('other page');
             window.location.href = 'https://search.bilibili.com/';
         }
-    }
-    // for other pages, redirect to search page
-    else {
-        console.log('other page');
-        window.location.href = 'https://search.bilibili.com/';
     }
 }
 
 (function() {
     'use strict';
+    // 只在顶层页面运行
+    if (window.top !== window.self) {
+        return;
+    }
     // add mainFunc to onload
     console.log('add mainFunc to onload');
     var oldonload = window.onload;
